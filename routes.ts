ad92d6e0e7c99b2264622837dd6ef2eb1ca23328
eskindercoop <139558213+eskindercoop@users.noreply.github.com>
2025-03-30 15:43:37 +0300

    old code

--- routes.ts
+++ routes.ts
@@ -1,26 +1,26 @@
-/**
- * These routes do not require authentication.
- * @type {string[]}
- */
-export const publicRoutes = ["/public"];
+// /**
+//  * These routes do not require authentication.
+//  * @type {string[]}
+//  */
+// export const publicRoutes = ["/public"];
 
-/**
- * An array of routes that are used for authentication.
- * These routes will redirect logged-in users to the dashboard.
- * @type {string[]}
- */
-export const authRoutes = [
-  "/auth/login",
-  "/register",
-  "/forgot-password",
-  "/reset-password",
-  "/",
-];
+// /**
+//  * An array of routes that are used for authentication.
+//  * These routes will redirect logged-in users to the dashboard.
+//  * @type {string[]}
+//  */
+// export const authRoutes = [
+//   "/auth/login",
+//   "/register",
+//   "/forgot-password",
+//   "/reset-password",
+//   "/",
+// ];
 
-export const apiAuthPrefix = "/api/auth";
+// export const apiAuthPrefix = "/api/auth";
 
-/**
- * The default redirect path after a successful login.
- * @type {string}
- */
-export const DEFAULT_LOGIN_REDIRECT = "/dashboard/index";
+// /**
+//  * The default redirect path after a successful login.
+//  * @type {string}
+//  */
+// export const DEFAULT_LOGIN_REDIRECT = "/dashboard/index";
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,79 +1,79 @@
-// import { cookies } from "next/headers";
-// import { NextRequest, NextResponse } from "next/server";
-
-import { NextRequest } from "next/server";
-import { publicRoutes, DEFAULT_LOGIN_REDIRECT, authRoutes } from "../routes";
 import { cookies } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
 
-// import { decrypt } from "@/lib/session";
+// import { NextRequest } from "next/server";
+// import { publicRoutes, DEFAULT_LOGIN_REDIRECT, authRoutes } from "../routes";
+// import { cookies } from "next/headers";
 
-// const protectedRoutes = ["/dashboard"];
-// const publicRoutes = ["/"];
+import { decrypt } from "@/lib/session";
 
-// export default async function middleware(req: NextRequest) {
-//   const path = req.nextUrl.pathname;
-//   const isProtectedRoute = protectedRoutes.includes(path);
-//   const isPublicRoute = publicRoutes.includes(path);
+const protectedRoutes = ["/dashboard"];
+const publicRoutes = ["/"];
 
-//   const cookie = cookies().get("session")?.value;
-//   if (!cookie) {
-//     // return NextResponse.redirect(new URL("/", req.nextUrl));
-//   } else {
-//     console.log("Cookie found", path);
-//   }
-//   const session = await decrypt(cookie);
-//   console.log("--------------", session);
-//   if (isProtectedRoute && !session?.token) {
-//     console.log("I am in protected route", path);
-//     return NextResponse.redirect(new URL("/login", req.nextUrl));
-//   }
+export default async function middleware(req: NextRequest) {
+  const path = req.nextUrl.pathname;
+  const isProtectedRoute = protectedRoutes.includes(path);
+  const isPublicRoute = publicRoutes.includes(path);
 
-//   if (isPublicRoute && session?.token && !path.startsWith("/dashboard")) {
-//     console.log("I am in public route", path);
-//     return NextResponse.redirect(new URL("/dashboard", req.nextUrl));
-//   }
-
-//   return NextResponse.next();
-// }
-
-// Routes Middleware should not run on
-// export const config = {
-//   matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
-// };
-
-export default function middleware(req: NextRequest) {
-  const { nextUrl } = req;
-  // const isLoggedIn = !!req.auth;
-  const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
-  const isAuthRoute = authRoutes.includes(nextUrl.pathname);
-  const isLoggedIn = !!cookies().get("session")?.value;
-  console.log("isLoggedIn", isLoggedIn);
-  if (isAuthRoute) {
-    if (isLoggedIn) {
-      return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
-    }
-    return null;
+  const cookie = cookies().get("session")?.value;
+  if (!cookie) {
+    // return NextResponse.redirect(new URL("/", req.nextUrl));
+  } else {
+    console.log("Cookie found", path);
+  }
+  const session = await decrypt(cookie);
+  console.log("--------------", session);
+  if (isProtectedRoute && !session?.token) {
+    console.log("I am in protected route", path);
+    return NextResponse.redirect(new URL("/", req.nextUrl));
   }
 
-  if (!isLoggedIn && !isPublicRoute) {
-    console.log("Not logged in", nextUrl.pathname);
-    console.log(
-      "next routing",
-      Response.redirect(new URL("/auth/login", nextUrl))
-    );
-    return Response.redirect(new URL("/auth/login", nextUrl));
+  if (isPublicRoute && session?.token && !path.startsWith("/dashboard")) {
+    console.log("I am in public route", path);
+    return NextResponse.redirect(new URL("/dashboard", req.nextUrl));
   }
 
-  return null;
-
-  // return NextResponse.next();
+  return NextResponse.next();
 }
 
+// Routes Middleware should not run on
 export const config = {
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    // Always run for API routes
-    "/(api|trpc)(.*)",
-  ],
+  matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
 };
+
+// export default function middleware(req: NextRequest) {
+//   const { nextUrl } = req;
+//   // const isLoggedIn = !!req.auth;
+//   const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+//   const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+//   const isLoggedIn = !!cookies().get("session")?.value;
+//   console.log("isLoggedIn", isLoggedIn);
+//   if (isAuthRoute) {
+//     if (isLoggedIn) {
+//       return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+//     }
+//     return null;
+//   }
+
+//   if (!isLoggedIn && !isPublicRoute) {
+//     console.log("Not logged in", nextUrl.pathname);
+//     console.log(
+//       "next routing",
+//       Response.redirect(new URL("/auth/login", nextUrl))
+//     );
+//     return Response.redirect(new URL("/auth/login", nextUrl));
+//   }
+
+//   return null;
+
+//   // return NextResponse.next();
+// }
+
+// export const config = {
+//   matcher: [
+//     // Skip Next.js internals and all static files, unless found in search params
+//     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+//     // Always run for API routes
+//     "/(api|trpc)(.*)",
+//   ],
+// };
